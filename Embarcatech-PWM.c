351ca1cfbde946346b6e36da1d46c4f182314966
EmyleSilva <emylesnt.silva@gmail.com>
2025-02-09 09:00:45 -0300

    Configuração inicial do projeto

---
+++ Embarcatech-PWM.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/time.h"
+#include "hardware/pwm.h"
+
+#define MOTOR_PIN 22
+
+int main()
+{
+    stdio_init_all();
+
+    while (true) {
+        
+    }
+}
